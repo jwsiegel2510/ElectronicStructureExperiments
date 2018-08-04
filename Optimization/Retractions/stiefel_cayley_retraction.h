7b7bf2d7c12ddbcfd6b6a8e620a1976ca4422683
Jonathan Siegel <jwsiegel@ucla.edu>
2018-08-04 14:53:34 -0400

    Started writing class for Cayley retraction on the Stiefel manifold.

---
+++ Optimization/Retractions/stiefel_cayley_retraction.h
@@ -0,0 +1,29 @@
+/* This file is part of an open source library for electronic structure calculations.
+ *  It contains a templated implementation of a retraction on the Stiefel manifold
+ *  based on the Cayley transform (alternatively on the (1,1) Pade approximation of
+ *  of the exponential. The classes passed as parameters are assumed to represent
+ *  points on the manifold and tangent vectors. 
+ *  
+ *  As such, instances of these classes must essentitally be matrices, 
+ *  supporting linear algebra operations, including solving linear systems.
+ *  
+ *  Copyright (C) 2018 Jonathan W. Siegel
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#ifndef _OPTIMIZATION_RETRACTIONS_STIEFEL_CAYLEY_RETRACTION__
+#define _OPTIMIZATION_RETRACTIONS_STIEFEL_CAYLEY_RETRACTION__
+
+#endif
