2b8f5ac180d283c95e833fce1db5f154d1864466
Jonathan Siegel <jwsiegel@ucla.edu>
2018-08-04 15:26:29 -0400

    Added implementation of the Cayley retraction on the Stiefel manifold.

--- Optimization/Retractions/stiefel_cayley_retraction.h
+++ Optimization/Retractions/stiefel_cayley_retraction.h
@@ -1,11 +1,13 @@
 /* This file is part of an open source library for electronic structure calculations.
+ *  
  *  It contains a templated implementation of a retraction on the Stiefel manifold
  *  based on the Cayley transform (alternatively on the (1,1) Pade approximation of
  *  of the exponential. The classes passed as parameters are assumed to represent
- *  points on the manifold and tangent vectors. 
+ *  points on the manifold and dual tangent vectors. 
  *  
  *  As such, instances of these classes must essentitally be matrices, 
  *  supporting linear algebra operations, including solving linear systems.
+ *  The synthax is assumed to be the same as for Eigen matrices.
  *  
  *  Copyright (C) 2018 Jonathan W. Siegel
  *
@@ -25,5 +27,45 @@
 
 #ifndef _OPTIMIZATION_RETRACTIONS_STIEFEL_CAYLEY_RETRACTION__
 #define _OPTIMIZATION_RETRACTIONS_STIEFEL_CAYLEY_RETRACTION__
+
+namespace optimization {
+namespace retractions {
+
+template<class P, class V> class StiefelCayleyRetraction {
+private:
+	P U;
+	P VM;
+	V X;
+public:
+        void retract(P& iterate, const V& direction, double dt);
+        void extrapolate(P& output, const P& point_one, const P& point_two, double beta);
+        double norm_sq(const V& grad, const P& iterate);
+};
+
+template<class P, class V> void StiefelCayleyRetraction<P,V>::retract(P& iterate, const V& direction, double dt) {
+	U.resize(iterate.rows(), 2 * iterate.cols());
+	VM.resize(iterate.rows(), 2 * iterate.cols());
+	U.block(0, 0, iterate.rows(), iterate.cols()) = -0.5 * dt * direction;
+	U.block(0, iterate.cols(), iterate.rows(), iterate.cols()) = 0.5 * dt * iterate;
+	VM.block(0, 0, iterate.rows(), iterate.cols()) = iterate;
+	VM.block(0, iterate.cols(), iterate.rows(), iterate.cols()) = direction;
+	iterate = iterate - 2.0 * U * (MatrixXd::Identity(2 * iterate.cols(), 2 * iterate.cols()) + VM.transpose() * U).partialPivLu().solve(VM.transpose() * iterate);
+}
+
+template<class P, class V> double StiefelCayleyRetraction<P,V>::norm_sq(const V& grad, const P& iterate) {
+        X.resize(grad.rows(), grad.cols());
+	X = grad;
+	X -= iterate * (grad.transpose() * iterate);
+        return (X.transpose() * X).trace();
+}
+
+template<class P, class V> void StiefelCayleyRetraction<P,V>::extrapolate(P& output, const P& point_one, const P& point_two, double beta) {
+	X = 2.0 * (P::Identity(iterate.cols(), iterate.cols()) + point_two.transpose() * point_one).partialPivLu().solve(point_two.transpose()).transpose();
+	output = point_one;
+	retract(output, X, 1.0 + beta);
+}
+
+} // namespace retractions
+} // namespace optimization
 
 #endif
